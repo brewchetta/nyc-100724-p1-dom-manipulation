0c345af5bb247bf82bcfb81d220609bd3b9bdc27
brewchetta <chett.tiller@gmail.com>
2024-10-08 14:36:03 -0400

    init

---
+++ practice.js
@@ -0,0 +1,36 @@
+// // PAIR PROGRAMMING PRACTICE // //
+// fork and clone this repo and follow the instructions in this file
+
+// for all the instructions below work in this file, DO NOT amend the html to get your results, use DOM manipulation
+
+// // DELIVERABLES // //
+
+// 1. create a new div with an id of .movie-posters and append it to the body
+
+// 2. do this three times:
+
+// 2.1 find a movie poster you like and add it as an <img> inside the .movie-poster div
+
+// 2.2 beneath each <img> add the movie title as an <h2>
+
+// 2.3 further refine the movie posters:
+// each movie poster gets its own <div> which includes the poster <img>, the title <h2>
+// and a short description <p>
+
+// 4. console.log the title and description for the first movie poster?
+
+// 5. change the <img> src, title text, and description text for the second movie poster?
+
+// 6. delete the last movie poster
+
+
+
+// // HINTS // //
+
+// given that adding movie posters is a repetetive task, it may be easier to build
+// a function that takes in certain arguments and does the work for you...
+
+// remember that you need a few steps in order to finally see a dom element on the page:
+// create the element
+// change attributes like text or src
+// append the element
