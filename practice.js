6515f388fb604a48ee7fca092a6799b973b0be73
brewchetta <chett.tiller@gmail.com>
2024-10-09 13:52:23 -0400

    edited instructions for practice

--- practice.js
+++ practice.js
@@ -5,21 +5,20 @@
 
 // // DELIVERABLES // //
 
-// 1. create a new div with an id of .movie-posters and append it to the body
+// 1. create a new div with an id of movie-posters and append it to the body
 
 // 2. do this three times:
 
-// 2.1 find a movie poster you like and add it as an <img> inside the .movie-poster div
+// 2.1 find a movie poster you like and add it as an <img> inside the movie-posters div
 
 // 2.2 beneath each <img> add the movie title as an <h2>
 
 // 2.3 further refine the movie posters:
-// each movie poster gets its own <div> which includes the poster <img>, the title <h2>
-// and a short description <p>
+// change it so each movie poster gets its own <div> which includes the poster <img>, the title <h2> and a short description <p>
 
-// 4. console.log the title and description for the first movie poster?
+// 4. console.log the title and description for the first movie poster
 
-// 5. change the <img> src, title text, and description text for the second movie poster?
+// 5. change the <img> src, title text, and description text for the second movie poster
 
 // 6. delete the last movie poster
 
